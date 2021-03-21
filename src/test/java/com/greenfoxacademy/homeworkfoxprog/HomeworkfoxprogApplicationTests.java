7d8c6a428c8c6b4f0670ca0a7386fec94fa14a7b
sisanny <woody.anny@gmail.com>
2021-03-21 11:18:56 +0100

    gitigrnore added

---
+++ src/test/java/com/greenfoxacademy/homeworkfoxprog/HomeworkfoxprogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.homeworkfoxprog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkfoxprogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
